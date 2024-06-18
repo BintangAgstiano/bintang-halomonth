df2382ca11f09ecf836aba4e89e3a1861b257669
Bintang Agustiano <bintangagustiano3@gmail.com>
2024-06-18 18:04:28 +0700

    modified

--- src/page/HistoryPage.jsx
+++ src/page/HistoryPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons'
 const HistoryPage = () => {
     return (
         <>
@@ -18,70 +19,86 @@ const HistoryPage = () => {
                         </div>
                     </div>
                     <div className="content-history">
-                        <div className="row d-flex flex-column gap-3" >
-                        <div className="col-10 parent-clear">
-                            <button className="btn-clear">Bersihkan</button>
+                        <div className="row d-flex flex-column gap-3 " >
+                            <div className="col-10 parent-clear">
+                                <button className="btn-clear">Bersihkan</button>
                             </div>
-                            <div className="box-content-history col-10">
-                                <div className="imgH">
-                                    <img src="/clock-history.png" width="80" />
+                            <div className="parent-box-ch">
+                                <div className="d-flex" style={{ width: 'max-content' }}>
+                                    <div className="circle-tes"></div>
+
                                 </div>
-                               <div className="tanggal">Tanggal :</div>
-                               <div className="content-tanggal">Senin,27 Agustus 2024</div>
+                                <div className="box-content-history col-8">
+                                    <div className="imgH">
+                                        <img src="/clock-history.png" width="80" />
+                                    </div>
+                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
+
+                                    <div className="tanggal">Tanggal :</div>
+                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
+                                </div>
                             </div>
-                            <div className="line-history col-10">
+                            <div className="parent-box-ch mt-5">
+                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
+                                    <div className="content-line"></div>
+                                    <div className="circle-tes"></div>
+
+                                </div>
+                                <div className="box-content-history col-8">
+                                    <div className="imgH">
+                                        <img src="/clock-history.png" width="80" />
+                                    </div>
+                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
+
+                                    <div className="tanggal">Tanggal :</div>
+                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
+                                </div>
+                            </div>
+                            <div className="parent-box-ch mt-5">
+                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
+                                    <div className="content-line"></div>
+                                    <div className="circle-tes"></div>
+
+                                </div>
+                                <div className="box-content-history col-8">
+                                    <div className="imgH">
+                                        <img src="/clock-history.png" width="80" />
+                                    </div>
+                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
+
+                                    <div className="tanggal">Tanggal :</div>
+                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
+                                </div>
+                            </div>
+                            <div className="parent-box-ch mt-5">
+                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
+                                    <div className="content-line"></div>
+                                    <div className="circle-tes"></div>
+
+                                </div>
+                                <div className="box-content-history col-8">
+                                    <div className="imgH">
+                                        <img src="/clock-history.png" width="80" />
+                                    </div>
+                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
+
+                                    <div className="tanggal">Tanggal :</div>
+                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
+                                </div>
+                            </div>
+                            {/* <div className="line-history col-10">
                                 <div className="content-line">
                                     <div className="box-text">
                                         <div className="box-dalam">24 hari
-                                        
-                                        <FontAwesomeIcon icon={faCaretRight} style={{position:'absolute', transform:"scale(2)",left:'75px',color:"rgb(45, 47, 66)" }}></FontAwesomeIcon>
+
+                                            <FontAwesomeIcon icon={faCaretRight} style={{ position: 'absolute', transform: "scale(2)", left: '75px', color: "rgb(45, 47, 66)" }}></FontAwesomeIcon>
                                         </div>
                                     </div>
                                     <div className="circle1H"></div>
                                     <div className="circle2H"></div>
                                 </div>
-                            </div>
-                            <div className="box-content-history col-10">
-                                <div className="imgH">
-                                    <img src="/clock-history.png" width="80" />
-                                </div>
-                                <div className="tanggal">Tanggal :</div>
-                               <div className="content-tanggal">Senin,27 Agustus 2024</div>
-                            </div>
-                            <div className="line-history col-10">
-                                <div className="content-line">
-                                <div className="box-text">
-                                        <div className="box-dalam">24 hari
-                                        
-                                        <FontAwesomeIcon icon={faCaretRight} style={{position:'absolute', transform:"scale(2)",left:'75px',color:"rgb(45, 47, 66)" }}></FontAwesomeIcon>
-                                        </div>
-                                    </div>
-                                    <div className="circle1H"></div>
-                                    <div className="circle2H"></div>
-                                </div>
-                            </div>
-                            <div className="box-content-history col-10">
-                                <div className="imgH">
-                                    <img src="/clock-history.png" width="80" />
-                                </div>
-                            </div>
-                            <div className="line-history col-10">
-                                <div className="content-line">
-                                <div className="box-text">
-                                        <div className="box-dalam">24 hari
-                                        
-                                        <FontAwesomeIcon icon={faCaretRight} style={{position:'absolute', transform:"scale(2)",left:'75px',color:"rgb(45, 47, 66)" }}></FontAwesomeIcon>
-                                        </div>
-                                    </div>
-                                    <div className="circle1H"></div>
-                                    <div className="circle2H"></div>
-                                </div>
-                            </div>
-                            <div className="box-content-history col-10">
-                                <div className="imgH">
-                                    <img src="/clock-history.png" width="80" />
-                                </div>
-                            </div>
+                            </div> */}
+
                         </div>
                     </div>
                     <section id="footer-history">
