b42fd0d4b40ac55ded063b26fa54412065ff20e4
Bintang Agustiano <bintangagustiano3@gmail.com>
2024-06-19 20:41:37 +0700

    modified

--- src/page/HistoryPage.jsx
+++ src/page/HistoryPage.jsx
@@ -2,9 +2,55 @@ import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { faCaretLeft } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from "react"
+import { format } from "date-fns";
+import { id } from "date-fns/locale";
+import Swal from "sweetalert2"; // Import SweetAlert2
+
 const HistoryPage = () => {
+    const [getDate, setDate] = useState([]);
+    useEffect(() => {
+        const storedHistory = localStorage.getItem('history');
+        if (storedHistory) {
+
+            let dateHistory = JSON.parse(storedHistory);
+            setDate(dateHistory)
+        }
+    }, []);
+
+    function handleClear() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Do you want to delete history?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, submit it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.removeItem('history')
+                setDate([])
+        Swal.fire({
+            title: 'Submitted!',
+            text: 'Your history has been delete.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire({
+            title: 'Cancelled',
+            text: 'Your submission has been cancelled.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+})
+    }
     return (
         <>
+        
+        
             <div className="containerr">
                 <section id="sectionHistory">
                     <div className="navbar d-flex align-items-center justify-content-between">
@@ -18,89 +64,90 @@ const HistoryPage = () => {
                             <Link to={'/'}> <button type="button" class="btn">Kembali</button></Link>
                         </div>
                     </div>
-                    <div className="content-history">
-                        <div className="row d-flex flex-column gap-3 " >
-                            <div className="col-10 parent-clear">
-                                <button className="btn-clear">Bersihkan</button>
-                            </div>
-                            <div className="parent-box-ch">
-                                <div className="d-flex" style={{ width: 'max-content' }}>
-                                    <div className="circle-tes"></div>
-
-                                </div>
-                                <div className="box-content-history col-8">
-                                    <div className="imgH">
-                                        <img src="/clock-history.png" width="80" />
+                    {getDate.length > 0 &&
+                        <div className="content-history">
+                            <div className="row d-flex flex-column gap-3 " >
+                                {getDate.length > 0 ? (
+                                    <div className="col-10 parent-clear">
+                                        <button onClick={() => handleClear()} className="btn-clear">Bersihkan</button>
                                     </div>
-                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
+                                ) : ('')}
 
-                                    <div className="tanggal">Tanggal :</div>
-                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
-                                </div>
+                                {getDate.map((item, index) => {
+                                    const formattedDate = format(new Date(item), 'EEEE, d MMMM yyyy', { locale: id }); // Memformat tanggal
+                                    return (
+                                        <>
+
+                                            {index == 0 ? (
+                                                // Untuk data pertama tanpa garis
+                                                <div className="parent-box-ch mb-4" >
+                                                    <div className="d-flex" style={{ width: 'max-content' }}>
+                                                        <div className="circle-tes"></div>
+                                                    </div>
+                                                    <div className="box-content-history col-8">
+                                                        <div className="imgH">
+                                                            <img src="/clock-history.png" width="80" alt="clock history" />
+                                                        </div>
+                                                        <FontAwesomeIcon
+                                                            icon={faCaretLeft}
+                                                            style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}
+                                                        />
+                                                        <div className="tanggal">Tanggal :</div>
+                                                        <div className="content-tanggal">{formattedDate}</div>
+                                                    </div>
+                                                </div>
+                                            ) : (
+                                                // Untuk data kedua dan seterusnya dengan garis
+                                                <>
+                                                    <div className="parent-box-ch mt-4" >
+                                                        <div className="d-flex" style={{ width: 'max-content', position: 'relative' }}>
+                                                            <div className="content-line"></div>
+                                                            <div className="circle-tes"></div>
+                                                        </div>
+                                                        <div className="box-content-history col-8">
+                                                            <div className="imgH">
+                                                                <img src="/clock-history.png" width="80" alt="clock history" />
+                                                            </div>
+                                                            <FontAwesomeIcon
+                                                                icon={faCaretLeft}
+                                                                style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}
+                                                            />
+                                                            <div className="tanggal">Tanggal :</div>
+                                                            <div className="content-tanggal">{formattedDate}</div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="line-history col-10">
+                                                        <div className="content-line">
+                                                            <div className="box-text">
+                                                                <div className="box-dalam">24 hari
+                                                                    <FontAwesomeIcon
+                                                                        icon={faCaretRight}
+                                                                        style={{ position: 'absolute', transform: "scale(2)", left: '75px', color: "rgb(45, 47, 66)" }}
+                                                                    />
+                                                                </div>
+                                                            </div>
+                                                            <div className="circle1H"></div>
+                                                            <div className="circle2H"></div>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )}
+
+                                        </>
+                                    );
+                                })}
+
+
+
+
                             </div>
-                            <div className="parent-box-ch mt-5">
-                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
-                                    <div className="content-line"></div>
-                                    <div className="circle-tes"></div>
-
-                                </div>
-                                <div className="box-content-history col-8">
-                                    <div className="imgH">
-                                        <img src="/clock-history.png" width="80" />
-                                    </div>
-                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
-
-                                    <div className="tanggal">Tanggal :</div>
-                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
-                                </div>
-                            </div>
-                            <div className="parent-box-ch mt-5">
-                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
-                                    <div className="content-line"></div>
-                                    <div className="circle-tes"></div>
-
-                                </div>
-                                <div className="box-content-history col-8">
-                                    <div className="imgH">
-                                        <img src="/clock-history.png" width="80" />
-                                    </div>
-                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
-
-                                    <div className="tanggal">Tanggal :</div>
-                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
-                                </div>
-                            </div>
-                            <div className="parent-box-ch mt-5">
-                                <div className="d-flex" style={{ width: 'max-content',position:'relative',}}>
-                                    <div className="content-line"></div>
-                                    <div className="circle-tes"></div>
-
-                                </div>
-                                <div className="box-content-history col-8">
-                                    <div className="imgH">
-                                        <img src="/clock-history.png" width="80" />
-                                    </div>
-                                    <FontAwesomeIcon icon={faCaretLeft} style={{ position: 'absolute', transform: "scale(3)", top: '58px', left: '-5px', color: "white" }}></FontAwesomeIcon>
-
-                                    <div className="tanggal">Tanggal :</div>
-                                    <div className="content-tanggal">Senin,27 Agustus 2024</div>
-                                </div>
-                            </div>
-                            {/* <div className="line-history col-10">
-                                <div className="content-line">
-                                    <div className="box-text">
-                                        <div className="box-dalam">24 hari
-
-                                            <FontAwesomeIcon icon={faCaretRight} style={{ position: 'absolute', transform: "scale(2)", left: '75px', color: "rgb(45, 47, 66)" }}></FontAwesomeIcon>
-                                        </div>
-                                    </div>
-                                    <div className="circle1H"></div>
-                                    <div className="circle2H"></div>
-                                </div>
-                            </div> */}
-
                         </div>
-                    </div>
+                    }
+                    {getDate.length === 0 &&
+                        <div className="content-history d-flex justify-content-center align-items-center border">
+                            <p style={{ color:'gray' }}>Tidak ada history</p>
+                        </div>
+                    }
                     <section id="footer-history">
                         <div className="box-footer">Copyright ©2024 Bintang Agustiano Effendy</div>
                     </section>
--- src/page/PageIndex.jsx
+++ src/page/PageIndex.jsx
@@ -1,91 +1,196 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
-
+import { format } from "date-fns";
+import { id } from "date-fns/locale";
+import Swal from "sweetalert2"; // Import SweetAlert2
 const PageIndex = () => {
+    const [dateList, setDate] = useState(() => {
+        let storage = localStorage.getItem('history');
+        if (storage) {
+            return JSON.parse(storage)
+
+        } else {
+            return []
+        }
+    })
+    const [showHalomonth,setHalomonth]=useState(true);
+    const [dateNow, setDateNow] = useState();
+    useEffect(() => {
+        let newDate = new Date()
+        let date = newDate.getDate();
+        let month = (newDate.getMonth() + 1).toString().padStart(2, 0);
+        let year = newDate.getFullYear();
+        let dateCurrent = `${year}-${month}-${date}`
+       
+        const formattedDate = format(new Date(dateCurrent), 'EEEE, d MMMM yyyy', { locale: id }); // Memformat tanggal
+        setDateNow(formattedDate)
+        setTimeout(() => {
+            setHalomonth(false);
+        }, 1500);
+    }, [])
+    function handleToday() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Do you want to submit today's date?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, submit it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+        let newDate = new Date()
+        let date = newDate.getDate();
+        let month = (newDate.getMonth() + 1).toString().padStart(2, 0);
+        let year = newDate.getFullYear();
+        let dateCurrent = `${year}-${month}-${date}`
+        let arrDate = [dateCurrent, ...dateList]
+        setDate(arrDate);
+        localStorage.setItem('history', JSON.stringify(arrDate));
+        Swal.fire({
+            title: 'Submitted!',
+            text: 'Your date has been submitted.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire({
+            title: 'Cancelled',
+            text: 'Your submission has been cancelled.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+});
+    }
+    function handleChangeToday(e) {
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Do you want to submit this date?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, submit it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+        let data = e.target.date.value
+        let arrDate2 = [data, ...dateList]
+        setDate(arrDate2);
+        localStorage.setItem('history', JSON.stringify(arrDate2));
+        Swal.fire({
+            title: 'Submitted!',
+            text: 'Your date has been submitted.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire({
+            title: 'Cancelled',
+            text: 'Your submission has been cancelled.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+});
+    }
     return (
-
-        <div className="containerr">
-            <section id="section1">
-                <div className="navbar d-flex align-items-center justify-content-between">
-                    <div className="left-navbar d-flex align-items-center" >
-                        <img src="/logo.png" width="30" />
-                        <div className="text-navbar">
-                            halomonth
-                        </div>
-                    </div>
-                    <div className="right-navbar">
-                       <Link to={'/history'}> <button type="button" class="btn">History</button></Link>
-                    </div>
-                </div>
-            </section>
-            <section id="section2" className=" row-12 mt-5 mb-5">
-                <div className=" boxPeringatan ml-1">
-                    <div className=" col-11 col-lg-5 col-md-8 col-sm-8 mt-3 p-3 content d-flex align-items-center">
-                        <img src="/warning.png" className="img " />
-                        <h6 className="mt-2 ">Peringatan!! Harus memilih salah satu di bawah ini</h6>
-                    </div>
-                </div>
-            </section>
-            <section id="section3" className="mt-5">
-                <div className="container-sc3 ">
-                    <div className="content-s3 col-12">
-
-                        <div className="box-circle">
-                            <div className="circle1"></div>
-                            <div className="circle2"></div>
-                            <div className="circle3"></div>
-                        </div>
-                        <div className="hari-ini d-flex">
-                            <img src="/clock.png" className="img-clock" width="80" />
-                            <div className="date d-flex flex-column" >
-                                <span className="title-hari-ini">HARI INI :</span>
-                                <span className="date-hari-ini">Senin,27 Agustus 2024</span>
+        <>
+            <div className="containerr">
+                <section id="section1">
+                    <div className="navbar d-flex align-items-center justify-content-between">
+                        <div className="left-navbar d-flex align-items-center" >
+                            <img src="/logo.png" width="30" />
+                            <div className="text-navbar">
+                                halomonth
                             </div>
                         </div>
-                        <button className="btn2">✓ Submit</button>
-
-                    </div>
-                </div>
-            </section>
-            <section id="section-line">
-                <div className="box-line">
-                    <span></span>
-                    <span>ATAU</span>
-                    <span></span>
-                </div>
-            </section>
-            <section id="section4" className="mt-5">
-                <div className="container-sc3 ">
-                    <div className="content-s3  col-12">
-
-                        <div className="box-circle">
-                            <div className="circle1"></div>
-                            <div className="circle2"></div>
-                            <div className="circle3"></div>
+                        <div className="right-navbar">
+                            <Link to={'/history'}> <button type="button" class="btn">History</button></Link>
                         </div>
-                        <div className="hari-ini  d-flex" style={{ marginLeft: "-5px" }}>
-                            <img src="/women.png" className="img-clock" width="120" />
-                            <div className="date d-flex flex-column" >
-                                <span className="title-hari-ini mt-2">SEARCH TANGGAL :</span>
-                                <br />
+                    </div>
+                </section>
+                <section id="section2" className=" row-12 mt-5 mb-5">
+                    <div className=" boxPeringatan ml-1">
+                        <div className=" col-11 col-lg-5 col-md-8 col-sm-8 mt-3 p-3 content d-flex align-items-center">
+                            <img src="/warning.png" className="img " />
+                            <h6 className="mt-2 ">Peringatan!! Harus memilih salah satu di bawah ini</h6>
+                        </div>
+                    </div>
+                </section>
+                <section id="section3" className="mt-5">
+                    <div className="container-sc3 ">
+                        <div className="content-s3 col-12">
 
-                                <div class="input-group mb-3" >
-                                    <span class="input-group-text" id="basic-addon1"><img src="/timer.png" width="20" /></span>
-                                    <input type="date" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
-
+                            <div className="box-circle">
+                                <div className="circle1"></div>
+                                <div className="circle2"></div>
+                                <div className="circle3"></div>
+                            </div>
+                            <div className="hari-ini d-flex">
+                                <img src="/clock.png" className="img-clock" width="80" />
+                                <div className="date d-flex flex-column" >
+                                    <span className="title-hari-ini">HARI INI :</span>
+                                    <span className="date-hari-ini">{dateNow}</span>
                                 </div>
-
                             </div>
-                        </div>
-                        <button className="btn2">✓ Submit</button>
+                            <button className="btn2" onClick={() => handleToday()}>✓ Submit</button>
 
+                        </div>
+                    </div>
+                </section>
+                <section id="section-line">
+                    <div className="box-line">
+                        <span></span>
+                        <span>ATAU</span>
+                        <span></span>
+                    </div>
+                </section>
+                <section id="section4" className="mt-5">
+                    <div className="container-sc3 ">
+                        <div className="content-s3  col-12">
+
+                            <div className="box-circle">
+                                <div className="circle1"></div>
+                                <div className="circle2"></div>
+                                <div className="circle3"></div>
+                            </div>
+                            <form onSubmit={handleChangeToday}>
+                                <div className="hari-ini  d-flex" style={{ marginLeft: "-5px" }}>
+                                    <img src="/women.png" className="img-clock" width="120" />
+                                    <div className="date d-flex flex-column" >
+                                        <span className="title-hari-ini mt-2">SEARCH TANGGAL :</span>
+                                        <br />
+                                        <div class="input-group mb-3" >
+                                            <span class="input-group-text" id="basic-addon1"><img src="/timer.png" width="20" /></span>
+                                            <input required type="date" name="date" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <button className="btn2">✓ Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+                <section id="footer">
+                    <div className="box-footer">Copyright ©2024 Bintang Agustiano Effendy</div>
+                </section>
+{showHalomonth &&
+                <div className="halomonth d-flex justify-content-center align-items-center gap-2" >
+                    <div className="lineRotate"></div>
+                    <img src="/logo-white.png" width="70" height={60} />
+                    <div className="d-flex flex-column">
+                        <div className="title-halomonth">halomonth</div>
+                        <span className="byName" >By Bintang Agustiano Effendy</span>
                     </div>
                 </div>
-            </section>
-            <section id="footer">
-                <div className="box-footer">Copyright ©2024 Bintang Agustiano Effendy</div>
-            </section>
-        </div>
-
+}
+            </div>
+        </>
     )
 }
 export default PageIndex
